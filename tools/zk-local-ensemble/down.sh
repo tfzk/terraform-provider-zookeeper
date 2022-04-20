24f18f5d17a402542aa21ec4fe3bdbd32a5113c7
Ivan De Marino <ivan.de.marino@gmail.com>
2022-04-21 00:28:46 +0100

    Updated tools for starting the zk-local-ensemble to use `docker compose` instead of `docker-compose`

--- tools/zk-local-ensemble/down.sh
+++ tools/zk-local-ensemble/down.sh
@@ -4,4 +4,4 @@ set -euo pipefail
 
 SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
 
-docker-compose -f ${SCRIPT_DIR}/docker-compose.yml down --remove-orphans
+docker compose -f ${SCRIPT_DIR}/docker-compose.yml down --remove-orphans
--- tools/zk-local-ensemble/up.sh
+++ tools/zk-local-ensemble/up.sh
@@ -4,4 +4,4 @@ set -euo pipefail
 
 SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
 
-docker-compose -f ${SCRIPT_DIR}/docker-compose.yml up --detach
+docker compose -f ${SCRIPT_DIR}/docker-compose.yml up --detach

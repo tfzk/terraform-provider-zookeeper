5a7b20db032aa10e4cb952ae1d67869169a4edd2
Patrik Šimunič <psimunic@dnanexus.com>
2026-05-23 20:29:21 +0200

    update examples to reflect latest changes

--- examples/provider/with_TLS/provider.tf
+++ examples/provider/with_TLS/provider.tf
@@ -1,6 +1,6 @@
 provider "zookeeper" {
-  servers               = "zk-server-01:2182,zk-server-02:2182"
-  session_timeout       = 30
-  tls_enable            = true
-  tls_root_ca_cert_path = "/path/to/root_CA_certificate.pem"
+  servers         = "zk-server-01:2182,zk-server-02:2182"
+  session_timeout = 30
+  tls_enabled     = true
+  tls_ca_file     = "/path/to/ca_cert.pem"
 }
